8a92b121714b7cb315bc931f8c5cbb52bc5807b0
ABINANDA-23EIR002 <you@example.com>
2025-06-19 14:53:22 +0530

    array topics

---
+++ array.js
@@ -0,0 +1,34 @@
+let fruits = ["apple", "watermelon", "grapes"];
+console.log("Original Array:");
+console.log(fruits);
+fruits.push("kiwi");
+console.log("Array after push operation:");
+console.log(fruits);
+fruits.pop();
+console.log("Array after pop operation:");
+console.log(fruits);
+
+console.log("Array the 1st & last element:");
+console.log(fruits[0]);
+let size = fruits.length;
+console.log(fruits[size - 1]);
+
+console.log("Array after shift operation");
+fruits.unshift("cherry");
+console.log(fruits);
+
+console.log("Array after shift operation");
+fruits.shift();
+console.log(fruits);
+
+console.log("Array after shift operation");
+let removedElement = fruits.splice(1, 1);
+console.log(fruits);
+
+fruits.push("orange");
+fruits.push("banana");
+fruits.push("mango");
+console.log("Accessing an array using loop:");
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
