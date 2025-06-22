9749f490b3f6cc84ce7279b2977c992df051db6a
ABINANDA-23EIR002 <you@example.com>
2025-06-22 09:41:04 +0530

    cpomplete datatype

--- data-types.js
+++ data-types.js
@@ -1,5 +1,15 @@
-let name="John Don";
+/*let name="John Don";
 let age=36;
 let price=150.36;
 let isStudent=false;
+console.log(name);
+
+let newString=`My name is ${name}`;
+console.log(newString);
+console.log(`$ ${price}`);*/
+let name = "John Doe";
+let age = 25;
+let price = 150.25;
+let isStudent = false;
+
 console.log(name);
